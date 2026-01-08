ac6dac143f3b9a4a0df568a334e7e0e02717e765
nik2005 <nikhilbhaskar2005@gmail.com>
2026-01-08 23:53:51 +0530

    feat: implement UTS namespace isolation
    Adds the 'nix' crate dependency. Uses unshare(CLONE_NEWUTS) to create a new UTS namespace and sets the hostname to 'crab-container'. This ensures the child process has an isolated hostname.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser,Subcommand};
 use std::process::Command;
+use nix::sched::{unshare, CloneFlags};
+use nix::unistd::sethostname;
 
 #[derive(Parser)]
 #[command(name = "crab-can")]
@@ -26,6 +28,12 @@ fn main() {
     
     match &cli.command{
     	Commands::Run{ cmd , args} => {
+    	
+    		println!("Output: setting up container isolation...");
+    		unshare(CloneFlags::CLONE_NEWUTS).expect("failed to unshare UTS namespace (did you run with sudo?)");
+    		sethostname("crab-containers").expect("failed to set hostname");
+    		
+    		
     		println!("Output : Preparing to run '{}' with args {:?}", cmd, args);
     		
     		let mut child = Command::new(cmd);
